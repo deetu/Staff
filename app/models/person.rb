5865fd6181f833e591e1389e467369fdcde1cecb
Bada Adedayo <deetu02@gmail.com>
2012-03-14 22:33:12 +0000

    Unit Test

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,15 +1,21 @@
 class Person < ActiveRecord::Base
-  attr_accessible :first_name, :surname, :other_names, :dob, :date_of_employment,:date_of_resignation, :salary_id, :email, :telephone, :title_id, :gender_id, :image
-  
-  
+  #attr_accessible :first_name, :surname, :other_names, :dob, :date_of_employment,:date_of_resignation, :salary_id, :email, :telephone, :title_id, :gender_id, :image
   belongs_to :salary
   belongs_to :title
   belongs_to :gender
   has_many :comments, :dependent => :destroy
-  validates :date_of_employment, :presence => true
+  
+  validates :first_name, :surname, :dob, :date_of_employment, :salary_id, :title_id, :gender_id,  :email, :image, :telephone, :presence => true
+  
+  validates :telephone, length: {minimum: 10}
+  
+  validates_format_of :email, :with => /^([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})$/i
+  
+  
   validate :date_of_employment_date_of_resignation
   mount_uploader :image, ImageUploader
-
+  
+   
   def date_of_employment_date_of_resignation
     errors.add(:date_of_employment, "must be before Date of Resignation") unless
        (self.date_of_employment).to_s > (self.date_of_resignation).to_s
--- test/unit/person_test.rb
+++ test/unit/person_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "person attributes must not be empty" do
+  person = Person.new
+  assert person.invalid?
+  assert person.errors[:first_name].any?
+  assert person.errors[:surname].any?
+  assert person.errors[:dob].any?
+  assert person.errors[:date_of_employment].any?
+  assert person.errors[:salary_id].any?
+  assert person.errors[:telephone].any?
+  assert person.errors[:title_id].any?
+  assert person.errors[:gender_id].any?
+  assert person.errors[:email].any?
+  assert person.errors[:image].any?
+  end
+  
+  def test_person_has_valid_fields
+    	person = Person.new
+    	assert !person.valid?
+  end
+  
+  def test_peron_has_valid_email
+    person = Person.new
+    assert !email.valid?
+  end
+  
+  def test_telephone_length
+    telephone = "xxxxxxxxx"
+    assert_equal(10, telephone.length)
+  end
 end
